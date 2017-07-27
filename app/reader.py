fdf402eb09e9e9dde05583d50075c0e97109111c
Edward Saavedra <edward@Edwards-MacBook-Pro.local>
2017-07-27 12:48:55 -0700

    Add full data set file

--- app/reader.py
+++ app/reader.py
@@ -1,6 +1,7 @@
 import os
 import film
 from nltk.tokenize import word_tokenize
+import csv
 
 SCRIPTS_FOLDER = 'scripts'
 
@@ -12,12 +13,31 @@ def script_files():
             sf.append(os.path.join(SCRIPTS_FOLDER, filename))
     return sf
 
-for s in script_files():
-    try:
-        f = film.Film(s)
-        print f.stats()
-    except IndexError:
-        print 'Bad script error: ' + s
+# for s in script_files():
+#     try:
+#         f = film.Film(s)
+#         print f.stats()
+#     except IndexError:
+#         print 'Bad script error: ' + s
+
+with open('script_data.csv','w') as datafile:
+    fieldnames = ['Title', 'Dialogue Ratio', 'Parenthetical Ratio', 'Words per Line', 'Dialogue Sentiment', 'Number of Scenes']
+    writer = csv.DictWriter(datafile, fieldnames=fieldnames)
+    tracker = 0
+    failures = 0
+    print 'Starting processing...'
+
+    writer.writeheader()
+    for s in script_files():
+        try:
+            f = film.Film(s)
+            writer.writerow(f.stats())
+            tracker += 1
+        except:
+            'Failed to write ' + s
+            failures += 1
+        print str(tracker) + ' successes and ' + str(failures) + ' failures...'
+    print 'Processing complete!'
 
 # for s in script_files():
 #     print s
