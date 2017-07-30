5dfaaab93a4a92181b2693f287166757ba4aba74
Edward Saavedra <edward@Edwards-MacBook-Pro.local>
2017-07-30 13:29:07 -0700

    Add axis labels to plots

--- app/visualize.py
+++ app/visualize.py
@@ -4,34 +4,44 @@ import csv
 class Visualize(object):
 
     def __init__(self):
-        self.dialogue_ratio = []
-        self.parenthetical_ratio = []
-        self.words_per_line = []
-        self.dialogue_sentiment = []
-        self.number_of_scenes = []
-        self.results = []
+        # self.dialogue_ratio = []
+        # self.parenthetical_ratio = []
+        # self.words_per_line = []
+        # self.dialogue_sentiment = []
+        # self.number_of_scenes = []
+        # self.results = []
+        self.data = {
+            'Dialogue Ratio':[],
+            'Parenthetical Ratio':[],
+            'Words per Line':[],
+            'Dialogue Sentiment':[],
+            'Number of Scenes':[],
+            'Results':[]
+        }
 
         with open('full_data_set.csv','rb') as f:
             reader = csv.reader(f)
             for r in reader:
                 try:
-                    self.dialogue_ratio.append(float(r[1]))
-                    self.parenthetical_ratio.append(float(r[2]))
-                    self.words_per_line.append(float(r[3]))
-                    self.dialogue_sentiment.append(float(r[4]))
-                    self.number_of_scenes.append(float(r[5]))
-                    self.results.append(float(r[8]))
+                    self.data['Dialogue Ratio'].append(float(r[1]))
+                    self.data['Parenthetical Ratio'].append(float(r[2]))
+                    self.data['Words per Line'].append(float(r[3]))
+                    self.data['Dialogue Sentiment'].append(float(r[4]))
+                    self.data['Number of Scenes'].append(float(r[5]))
+                    self.data['Results'].append(float(r[8]))
                 except:
                     print 'Skipping header line...'
 
     def view(self,field):
-        plt.plot(field,self.results,'bo')
+        plt.plot(self.data[field],self.data['Results'],'bo')
+        plt.xlabel(field)
+        plt.ylabel('Domestic Gross Revenue')
         plt.show()
 
     def view_all(self):
-        data = [self.dialogue_ratio, self.parenthetical_ratio, self.words_per_line, self.dialogue_sentiment, self.number_of_scenes]
-        for d in data:
-            self.view(d)
+        for d in self.data:
+            if d != 'Results':
+                self.view(d)
 
 v = Visualize()
 v.view_all()
