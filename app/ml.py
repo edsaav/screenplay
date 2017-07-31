283fc3fc084aa382d1462c8a09b60441140b41b4
Edward Saavedra <edward@Edwards-MacBook-Pro.local>
2017-07-30 19:46:29 -0700

    Add ML accuracy score logging

--- app/ml.py
+++ app/ml.py
@@ -1,38 +1,88 @@
 import numpy as np
 import tensorflow as tf
 import csv
+import time
 
-data = []
-results = []
+class Learner(object):
 
-with open('full_data_set.csv','rb') as f:
-    reader = csv.reader(f)
-    for r in reader:
-        try:
-            data_points = [float(r[1]),float(r[2]),float(r[3]),float(r[4]),float(r[5])]
-            data.append(data_points)
-            if float(r[8]) >= 5000000:
-                results.append(0)
-            else:
-                results.append(1)
-        except:
-            print 'Header line'
+    def __init__(self):
+        self.data = {
+            'Dialogue Ratio':[],
+            'Parenthetical Ratio':[],
+            'Words per Line':[],
+            'Dialogue Sentiment':[],
+            'Number of Scenes':[],
+            'Results':[],
+            'Results Category':[]
+        }
+        self.__read('full_data_set.csv')
 
-features = [tf.contrib.layers.real_valued_column('x', dimension=2)]
-estimator = tf.contrib.learn.LinearClassifier(feature_columns=features)
+    def linear_classify(self,column):
+        features = [tf.contrib.layers.real_valued_column('x', dimension=1)]
+        estimator = tf.contrib.learn.LinearClassifier(feature_columns=features)
 
-x_train = np.array(data[:300])
-y_train = np.array(results[:300])
-x_eval = np.array(data[301:])
-y_eval = np.array(results[301:])
-input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x_train}, y_train,
-                                              batch_size=300,
-                                              num_epochs=1000)
-eval_input_fn = tf.contrib.learn.io.numpy_input_fn(
-    {"x":x_eval}, y_eval, batch_size=121, num_epochs=1000)
+        x_train = np.array(self.data[column][:300])
+        y_train = np.array(self.data['Results Category'][:300])
+        x_eval = np.array(self.data[column][301:])
+        y_eval = np.array(self.data['Results Category'][301:])
+        input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x_train}, y_train,
+                                                      batch_size=300,
+                                                      num_epochs=1000)
+        eval_input_fn = tf.contrib.learn.io.numpy_input_fn(
+            {"x":x_eval}, y_eval, batch_size=121, num_epochs=1000)
 
-estimator.fit(input_fn=input_fn, steps=1000)
+        estimator.fit(input_fn=input_fn, steps=1000)
 
-accuracy_score = estimator.evaluate(input_fn=input_fn, steps=1)['accuracy']
+        accuracy_score = estimator.evaluate(input_fn=input_fn, steps=1)['accuracy']
 
-print "Accuracy Score: %r"% accuracy_score
+        self.__log(column,accuracy_score,'linear_classify')
+
+        print "Accuracy Score: %r"% accuracy_score
+
+    def dnn_classify(self,column):
+        features = [tf.contrib.layers.real_valued_column('x', dimension=1)]
+        estimator = tf.contrib.learn.DNNClassifier(feature_columns=features, hidden_units=[10,20,10],n_classes=2,model_dir='/tmp/iris_model')
+
+        x_train = np.array(self.data[column][:300])
+        y_train = np.array(self.data['Results Category'][:300])
+        x_eval = np.array(self.data[column][301:])
+        y_eval = np.array(self.data['Results Category'][301:])
+        input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x_train}, y_train,
+                                                      batch_size=300,
+                                                      num_epochs=1000)
+        eval_input_fn = tf.contrib.learn.io.numpy_input_fn(
+            {"x":x_eval}, y_eval, batch_size=121, num_epochs=1000)
+
+        estimator.fit(input_fn=input_fn, steps=1000)
+
+        accuracy_score = estimator.evaluate(input_fn=input_fn, steps=1)['accuracy']
+
+        self.__log(column,accuracy_score,'dnn_classify')
+
+        print "Accuracy Score: %r"% accuracy_score
+
+    def __read(self,source):
+        with open(source,'rb') as f:
+            reader = csv.reader(f)
+            for r in reader:
+                try:
+                    self.data['Dialogue Ratio'].append(float(r[1]))
+                    self.data['Parenthetical Ratio'].append(float(r[2]))
+                    self.data['Words per Line'].append(float(r[3]))
+                    self.data['Dialogue Sentiment'].append(float(r[4]))
+                    self.data['Number of Scenes'].append(float(r[5]))
+                    self.data['Results'].append(float(r[8]))
+                except:
+                    print 'Skipping header line...'
+            for r in self.data['Results']:
+                if (r) >= np.mean(self.data['Results']):
+                    self.data['Results Category'].append(1)
+                else:
+                    self.data['Results Category'].append(0)
+
+    def __log(self,column,accuracy,function):
+        with open('log.txt','a+') as f:
+            f.write(str(time.time()) + ' : ' + column + ' : ' + str(accuracy) + ' : ' + function + "\n")
+
+l = Learner()
+l.dnn_classify('Dialogue Ratio')
